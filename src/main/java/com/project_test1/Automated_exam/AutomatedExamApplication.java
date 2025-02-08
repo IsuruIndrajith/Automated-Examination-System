3b8083069e67b2838c84f77f74e6aaad870c31b1
Dinith132 <dinithp132@gamil.com>
2025-02-09 00:06:15 +0530

    Initialize Spring Boot project with basic configuration and structure

---
+++ src/main/java/com/project_test1/Automated_exam/AutomatedExamApplication.java
@@ -0,0 +1,13 @@
+package com.project_test1.Automated_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_test1/Automated_exam/AutomatedExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_test1.Automated_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
