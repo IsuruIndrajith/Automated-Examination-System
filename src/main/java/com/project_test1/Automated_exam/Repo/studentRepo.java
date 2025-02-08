1767f849646f5bfbac8c278b00eb0175bc81e96f
Dinith132 <dinithp132@gamil.com>
2025-02-09 00:14:40 +0530

    Add student service, repository, and controller classes

---
+++ src/main/java/com/project_test1/Automated_exam/Repo/studentRepo.java
@@ -0,0 +1,4 @@
+package com.project_test1.Automated_exam.Repo;
+
+public interface studentRepo {
+}
---
+++ src/main/java/com/project_test1/Automated_exam/Service/studentController.java
@@ -0,0 +1,4 @@
+package com.project_test1.Automated_exam.Service;
+
+public class studentController {
+}
