edf502fc6f866087e596060eab342677d17ec13d
Isuru <isuruindrajith680@gmail.com>
2025-02-16 23:32:51 +0530

    Student-Home Page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import StudentHome from "./pages/StudentHome";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/student-home" element={<StudentHome />} />
       </Routes>
     </Router>
   </React.StrictMode>
