492812e56244ba2a9b8fe6e8b69b9d87236082ed
Isuru <isuruindrajith680@gmail.com>
2025-03-21 17:16:18 +0530

    AdminHome

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login';
 import StudentHomepage from './pages/StudentHomePage';
+import AdminHome from './pages/AdminHome';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Login />} />
       <Route path="/student" element={<StudentHomepage />} />
+      <Route path="/admin" element={<AdminHome />} />
     </>
   )
 );
---
+++ Frontend/src/pages/AdminHome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavbarAdminHome from "../components/NavbarAdminHome"; 
+import './AdminHome.css';
+import { Link } from 'react-router-dom';
+
+function AdminHome() {
+  return (
+    <>
+    <NavbarAdminHome/>
+    
+    
+    </>
+    
+  )
+}
+
+export default AdminHome
