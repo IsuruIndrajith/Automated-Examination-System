910d55b32472ff6ec88ba2ee7feff4075e39b704
Isuru Indrajith <isuruindrajith680@gmail.com>
2025-03-27 10:57:56 +0530

    Update SidebarAdminHome.jsx

--- Frontend/src/components/SidebarAdminHome.jsx
+++ Frontend/src/components/SidebarAdminHome.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
@@ -34,40 +33,3 @@ export const SidebarData = [
   },
   
 ];
-=======
-import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
-import { MdQuiz } from "react-icons/md";
-import { FaStickyNote } from "react-icons/fa";
-import { CgDanger } from "react-icons/cg";
-
-export const SidebarData = [
-  {
-    title: 'Home',
-    path: '/',
-    icon: <AiIcons.AiFillHome size={20}  color='black' />,
-    cName: 'nav-text home-bg',
-  },
-  {
-    title: 'Manage',
-    path: '/manage',
-    icon: <IoIcons.IoIosPaper size={20} color='black' />,
-    cName: 'nav-text home-bg',
-  },
-  {
-    title: 'Departments',
-    path: '/departments',
-    icon: <IoIcons.IoMdPeople size={20} color='black' />,
-    cName: 'nav-text home-bg',
-  },
-  {
-      title: 'Notes',
-      path: '/notes',
-      icon: <FaStickyNote size={20} color='black'/>,
-      cName: 'nav-text home-bg',
-  },
-  
-];
->>>>>>> 97396686aa32552f05488ed25f526f48d153d683
