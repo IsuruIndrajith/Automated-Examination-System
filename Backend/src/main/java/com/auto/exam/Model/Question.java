a95232b5e6246d527975e7ad05ee66491df11ced
Dinith Priyanakara <154252977+Dinith132@users.noreply.github.com>
2025-04-04 19:44:19 +0530

    Update question model to include question type and modify sample data inserts

--- Backend/src/main/java/com/auto/exam/Model/Question.java
+++ Backend/src/main/java/com/auto/exam/Model/Question.java
@@ -1,9 +1,19 @@
 package com.auto.exam.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "question")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,59 +23,9 @@ public class Question {
     private String question;
     private Integer marks;
     private String answer;
+    private String questionType;
 
     @ManyToOne
     @JoinColumn(name = "exam_id")
     private Exam exam;
-
-    public Question() {
-    }
-
-    public Question(Long questionId, String question, Integer marks, String answer, Exam exam) {
-        this.questionId = questionId;
-        this.question = question;
-        this.marks = marks;
-        this.answer = answer;
-        this.exam = exam;
-    }
-
-    public Long getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(Long questionId) {
-        this.questionId = questionId;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public Integer getMarks() {
-        return marks;
-    }
-
-    public void setMarks(Integer marks) {
-        this.marks = marks;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public Exam getExam() {
-        return exam;
-    }
-
-    public void setExam(Exam exam) {
-        this.exam = exam;
-    }
 }
